cb4ca926c2af1d53627bc0b1a6cc106661884e62
Maksim Horbul <max_horbul@playstation.sony.com>
2011-01-06 11:58:44 -0800

    replace #missing_method with the dynamically defined_methods

--- lib/configurable/config.rb
+++ lib/configurable/config.rb
@@ -7,40 +7,25 @@ module Configurable
     end
 
     def method_missing(method, *args)
-      if method.match(/(.+)=$/)
-        self[$1] = args.first
-      else
-        self[method]
-      end
+      raise Errors::ConfigParamNotFound, "Configuration parameter #{method} is not found"
     end
 
     private
     def []=(key, value)
-      validate_key!(key.to_sym)
       @data[key.to_sym] = value.is_a?(Hash) ? self.class.new(value) : value
     end
 
     def [](key)
-      validate_key!(key.to_sym)
       @data[key.to_sym]
     end
 
     def update!(data)
-      data.each { |k,v| self[k] = v }
-      lock!
-    end
-
-    def lock!
-      @lock = true
-    end
-
-    def locked?
-      @lock == true
-    end
-
-    def validate_key!(key)
-      if locked? && !@data.keys.include?(key)
-        raise Errors::ConfigParamNotFound, "Configuration parameter #{key} is not found"
+      data.each do |k,v|
+        self[k] = v
+        self.class.class_eval do
+          define_method(k.to_sym) { self[k] } # def foo; self[foo]; end
+          define_method("#{k}=".to_sym) { |v| self[k] = v } # def foo=(v); self[foo]=v; end
+        end
       end
     end
   end
